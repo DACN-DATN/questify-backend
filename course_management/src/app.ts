72fcba6e16010a2a63de0054bc27b0f1a1dc7eba
HuyyTran <huytrn.dev@gmail.com>
2025-03-15 00:12:33 +0700

    fix: skaffold

--- course_management/src/app.ts
+++ course_management/src/app.ts
@@ -4,10 +4,10 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@datn242/questify-common';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
+// import { currentUserRouter } from './routes/current-user';
+// import { signinRouter } from './routes/signin';
+// import { signupRouter } from './routes/signup';
+// import { signoutRouter } from './routes/signout';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,10 +19,10 @@ app.use(
   }),
 );
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
+// app.use(currentUserRouter);
+// app.use(signinRouter);
+// app.use(signupRouter);
+// app.use(signoutRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
