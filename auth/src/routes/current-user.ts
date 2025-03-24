56c2f8165ffb3dd6f3c25838dea33f8818e08f88
NHA012 <nha@LAPTOP-QIB2U216.>
2025-03-24 13:22:17 +0000

    feat: change payload response in api currentuser

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 
 import { currentUser } from '@datn242/questify-common';
+import { User } from '../models/user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+router.get('/api/users/currentuser', currentUser, async (req, res) => {
+  let user = req.currentUser;
+  if (user) {
+    const response = await User.findOne({ _id: user.id });
+    if (response) {
+      res.send({ currentUser: response });
+    } else {
+      res.status(404).send({ error: 'User not found' });
+    }
+  } else {
+    res.send(user);
+  }
 });
 
 export { router as currentUserRouter };
