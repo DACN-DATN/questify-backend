dd902d779f51ca796704e8ea2a503b2f246f1186
NHA012 <nha@LAPTOP-QIB2U216.>
2025-03-25 05:31:58 +0000

    remove async

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,7 +4,7 @@ import { currentUser } from '@datn242/questify-common';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, async (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
 
