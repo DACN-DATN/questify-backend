a3931532453ddcbfffd29b98d5588d79a094ff45
Huy Tran <huytrn.dev@gmail.com>
2025-03-27 16:00:23 +0700

    refactor: resources

--- course_learning/src/routes/island/show.ts
+++ course_learning/src/routes/island/show.ts
@@ -1,33 +1,19 @@
 import express, { Request, Response } from 'express';
 import { NotFoundError } from '@datn242/questify-common';
 import { Level } from '../../models/level';
-import { Island } from '../../models/island';
 
 const router = express.Router();
 
-router.get(
-  '/api/course-learning/islands/:island_id/levels/:level_id',
-  async (req: Request, res: Response) => {
-    const { island_id, level_id } = req.params;
-    const island = await Island.findByPk(island_id);
+router.get('/api/course-learning/levels/:level_id', async (req: Request, res: Response) => {
+  const { level_id } = req.params;
 
-    if (!island) {
-      throw new NotFoundError();
-    }
+  const level = await Level.findByPk(level_id);
 
-    const level = await Level.findOne({
-      where: {
-        id: level_id,
-        islandId: island.id,
-      },
-    });
+  if (!level) {
+    throw new NotFoundError();
+  }
 
-    if (!level) {
-      throw new NotFoundError();
-    }
-
-    res.send(level);
-  },
-);
+  res.send(level);
+});
 
 export { router as showLevelRouter };
--- course_learning/src/routes/level/show_hints.ts
+++ course_learning/src/routes/level/show_hints.ts
@@ -1,15 +1,33 @@
 import express, { Request, Response } from 'express';
-import { NotFoundError, requireAuth } from '@datn242/questify-common';
+import {
+  BadRequestError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@datn242/questify-common';
 import { Level } from '../../models/level';
 import { Hint } from '../../models/hint';
 
 const router = express.Router();
 
 router.get(
-  '/api/course-learning/levels/:level_id/hints',
+  '/api/course-learning/hints',
   requireAuth,
+  [
+    // Validate that level-id exists and is a UUID
+    query('level-id')
+      .exists()
+      .withMessage('level-id is required')
+      .isUUID()
+      .withMessage('level-id must be a valid UUID'),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const { level_id } = req.params;
+    const level_id = req.query['level-id'] as string;
+
+    if (!level_id) {
+      throw new BadRequestError('Level ID is required.');
+    }
     const level = await Level.findByPk(level_id);
 
     if (!level) {
