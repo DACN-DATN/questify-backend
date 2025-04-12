09a740ca12198245cffba7fd9a744018feab444b
NHA012 <nha@LAPTOP-QIB2U216.>
2025-04-12 15:48:31 +0000

    feat: setup event

--- course_management/src/models/user.ts
+++ course_management/src/models/user.ts
@@ -1,6 +1,6 @@
 import { Model, DataTypes, Optional, ModelScopeOptions, ModelValidateOptions } from 'sequelize';
 import { sequelize } from '../config/db';
-import { UserRole } from '@datn242/questify-common';
+import { UserRole, UserStatus } from '@datn242/questify-common';
 import { v4 as uuidv4 } from 'uuid';
 
 const UserDefinition = {
@@ -10,39 +10,6 @@ const UserDefinition = {
     type: DataTypes.UUID,
     defaultValue: () => uuidv4(),
   },
-  gmail: {
-    allowNull: false,
-    type: DataTypes.STRING,
-    unique: true,
-    validate: {
-      isEmail: true,
-    },
-  },
-  hashedPassword: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-  phoneNumber: {
-    allowNull: true,
-    type: DataTypes.STRING,
-    validate: {
-      is: /^[0-9]{10,15}$/,
-    },
-  },
-  firstName: {
-    allowNull: true,
-    type: DataTypes.STRING,
-    validate: {
-      isAlpha: true,
-    },
-  },
-  lastName: {
-    allowNull: true,
-    type: DataTypes.STRING,
-    validate: {
-      isAlpha: true,
-    },
-  },
   role: {
     allowNull: false,
     type: DataTypes.STRING,
@@ -50,28 +17,27 @@ const UserDefinition = {
       isIn: [[UserRole.Student, UserRole.Teacher, UserRole.Admin]],
     },
   },
+  status: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    validate: {
+      isIn: [Object.values(UserStatus)],
+    },
+  },
 };
 
 interface UserAttributes {
   id: string;
-  gmail?: string;
-  hashedPassword: string;
-  phoneNumber?: string;
-  firstName?: string;
-  lastName?: string;
   role: UserRole;
+  status: UserStatus;
 }
 
 type UserCreationAttributes = Optional<UserAttributes, 'id'>;
 
 class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
   public id!: string;
-  public gmail?: string;
-  public hashedPassword!: string;
-  public phoneNumber?: string;
-  public firstName?: string;
-  public lastName?: string;
   public role!: UserRole;
+  public status!: UserStatus;
 
   static readonly scopes: ModelScopeOptions = {};
 
