589b97a40867f64d8708116b1f1db72edafed2e7
NHA012 <nha@LAPTOP-QIB2U216.>
2025-04-18 19:56:36 +0000

    feat: add events

--- course_learning/src/index.ts
+++ course_learning/src/index.ts
@@ -1,5 +1,6 @@
 import { app } from './app';
 import { connectDb, closeDbConnection } from './config/db';
+import { natsWrapper } from './nats-wrapper';
 import { syncModels } from './scripts/sync';
 
 const start = async () => {
@@ -18,6 +19,32 @@ const start = async () => {
     await closeDbConnection();
     process.exit(0);
   });
+
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 app.listen(3000, () => {
