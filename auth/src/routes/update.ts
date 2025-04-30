0290d489f2633186512c978147b96c2f553fda6c
Vu Luong <luongvu151003@gmail.com>
2025-04-30 09:54:51 +0700

    feat: integrate edit profile

--- auth/src/routes/update.ts
+++ auth/src/routes/update.ts
@@ -1,36 +1,44 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import {
-  requireAuth,
   validateRequest,
   NotFoundError,
   BadRequestError,
   NotAuthorizedError,
+  UserRole,
+  currentUser,
 } from '@datn242/questify-common';
 import { User } from '../models/user';
+import { UserUpdatedPublisher } from '../events/publishers/user-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.patch(
   '/api/users/:user_id',
-  requireAuth,
   [
     body('userName')
       .optional()
       .isLength({ min: 3 })
       .withMessage('Username must be at least 3 characters'),
     body('imageUrl').optional().isURL().withMessage('Image URL must be a valid URL'),
+    body('role')
+      .optional()
+      .isIn([UserRole.Student, UserRole.Teacher])
+      .withMessage('Role must be Student or Teacher'),
     body().custom((body) => {
-      if (!body.userName && !body.imageUrl) {
-        throw new Error('You must provide either userName or imageUrl to update');
+      if (!body.userName && !body.imageUrl && !body.role) {
+        throw new Error('You must provide either userName or imageUrl or role to update');
       }
       return true;
     }),
   ],
+  currentUser,
   validateRequest,
   async (req: Request, res: Response) => {
     const { user_id } = req.params;
-    const { userName, imageUrl } = req.body;
+    const { userName, imageUrl, role } = req.body;
     const currentUserId = req.currentUser!.id;
 
     if (currentUserId !== user_id) {
@@ -49,15 +57,47 @@ router.patch(
       if (existingUsername && existingUsername._id !== user_id) {
         throw new BadRequestError('Username is already in use');
       }
+
       user.userName = userName;
     }
 
-    // Update image URL if provided
     if (imageUrl) {
       user.imageUrl = imageUrl;
     }
 
-    await user.save();
+    if (role) {
+      user.role = role;
+    }
+
+    new UserUpdatedPublisher(natsWrapper.client).publish({
+      id: user.id,
+      role: user.role,
+      status: user.status,
+      gmail: user.email,
+      userName: user.userName,
+    });
+
+    try {
+      await user.save();
+
+      const userJwt = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          userName: user.userName,
+          role: user.role,
+          status: user.status,
+          imageUrl: user.imageUrl,
+        },
+        process.env.JWT_KEY!,
+      );
+
+      req.session = {
+        jwt: userJwt,
+      };
+    } catch (err) {
+      throw new BadRequestError('Failed to save user: ' + (err as Error).message);
+    }
 
     res.status(200).send(user);
   },
