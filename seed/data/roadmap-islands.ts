fc0a1ab8f22f7a3a98add1a548e7e90b35228174
HuyyTran <huytrn.dev@gmail.com>
2025-05-01 15:58:25 +0700

    fix: restructure seed files

--- seed/data/roadmap-islands.ts
+++ seed/data/roadmap-islands.ts
@@ -19,7 +19,7 @@ async function seed() {
     });
     console.log('Complete signup successful');
 
-    await api.patch(`/users/${teacherResponse.data.id}`, {
+    await api.patch(ResourcePrefix.Auth + `/${teacherResponse.data.id}`, {
       role: UserRole.Teacher,
     });
 
