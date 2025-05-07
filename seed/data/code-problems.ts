cf68693ba821555629b0d40ca6f942c1eb24d799
HuyyTran <huytrn.dev@gmail.com>
2025-05-07 22:04:57 +0700

    feat: seed testcases

--- seed/data/code-problems.ts
+++ seed/data/code-problems.ts
@@ -88,6 +88,30 @@ You can return the answer in any order.
     const codeProblem = codeProblemResponse.data;
     console.log('Code Problem seeded successfully:', codeProblem.id);
 
+    const testcaseResponse = await api.post(
+      ResourcePrefix.CodeProblem + `/${codeProblem.id}/testcases`,
+      {
+        testcases: [
+          {
+            input: 'nums = [2,7,11,15], target = 9',
+            output: '[0,1]',
+            hidden: false,
+          },
+          {
+            input: 'nums = [3,2,4], target = 6',
+            output: '[1,2]',
+            hidden: false,
+          },
+          {
+            input: 'nums = [3,3], target = 6',
+            output: '[0,1]',
+            hidden: true,
+          },
+        ],
+      },
+    );
+    console.log('Testcase seeded successfully:', testcaseResponse.data);
+
     await api.post(ResourcePrefix.Auth + '/signout', {});
     console.log('Teacher sign out successful');
   } catch (error) {
