c231a0291ceb308ef567374d772221148a8400a2
NHA012 <nha@LAPTOP-QIB2U216.>
2025-05-08 16:59:45 +0000

    fix lint

--- course_management/src/routes/course/index-instructor.ts
+++ course_management/src/routes/course/index-instructor.ts
@@ -4,10 +4,14 @@ import { requireAuth, ResourcePrefix } from '@datn242/questify-common';
 
 const router = express.Router();
 
-router.get(ResourcePrefix.CourseManagement + '/instructor', requireAuth, async (req: Request, res: Response) => {
-  const courses = await Course.findAll({ where: { teacherId: req.currentUser!.id } });
+router.get(
+  ResourcePrefix.CourseManagement + '/instructor',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const courses = await Course.findAll({ where: { teacherId: req.currentUser!.id } });
 
-  res.send(courses);
-});
+    res.send(courses);
+  },
+);
 
 export { router as indexCourseIntructorRouter };
