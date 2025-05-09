e592d8667bc69d5bea5472c5035404b46f4cc500
Vu Luong <luongvu151003@gmail.com>
2025-05-10 00:44:03 +0700

    feat: add filter status for index

---
+++ course_management/src/routes/course/index-admin.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import { Course } from '../../models/course';
+import { requireAuth, ResourcePrefix, CourseStatus, UserRole, NotAuthorizedError } from '@datn242/questify-common';
+import { Op } from 'sequelize';
+
+const router = express.Router();
+
+router.get(
+  ResourcePrefix.CourseManagement + '/admin',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    if (req.currentUser!.id !== UserRole.Admin) {
+        throw new NotAuthorizedError();
+    }
+
+    const courses = await Course.findAll({
+      where: {
+        status: {
+          [Op.in]: [CourseStatus.Approved, CourseStatus.Rejected, CourseStatus.Pending]
+        }
+      }
+    });
+
+    res.send(courses);
+  }
+);
+
+export { router as indexCourseAdminRouter };
--- course_management/src/routes/course/index.ts
+++ course_management/src/routes/course/index.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from 'express';
 import { Course } from '../../models/course';
-import { ResourcePrefix } from '@datn242/questify-common';
+import { ResourcePrefix, CourseStatus } from '@datn242/questify-common';
+import { Op } from 'sequelize';
 
 const router = express.Router();
 
 router.get(ResourcePrefix.CourseManagement, async (req: Request, res: Response) => {
-  const courses = await Course.findAll({});
+  const courses = await Course.findAll({
+    where: {
+      status: {
+        [Op.in]: [CourseStatus.Approved, CourseStatus.Draft, CourseStatus.Pending]
+      }
+    }
+  });
 
   res.send(courses);
 });
