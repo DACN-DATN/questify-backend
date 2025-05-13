ab75e02050f1142dfa425e9b49dc57c4feb33b1b
NHA012 <107928054+NHA012@users.noreply.github.com>
2025-05-13 18:03:21 +0700

    Fix/effect type (#84)
    * fix: effect type
    
    ---------
    
    Co-authored-by: NHA012 <nha@LAPTOP-QIB2U216>

--- course_learning/src/services/item-effect.service.ts
+++ course_learning/src/services/item-effect.service.ts
@@ -1,5 +1,5 @@
 import { EffectType, BadRequestError } from '@datn242/questify-common';
-import { Course } from '../models/course';
+import { UserCourse } from '../models/user-course';
 
 export function randomGoldItem(): number {
   return Math.floor(Math.random() * 190) + 10;
@@ -12,6 +12,7 @@ export function randomExpItem(): number {
 export async function multipleExpForNextLevel(
   effectType: EffectType,
   courseId: string,
+  userId: string,
 ): Promise<void> {
   const validExpEffects = [EffectType.ExpX2, EffectType.ExpX3, EffectType.ExpX4];
 
@@ -21,20 +22,26 @@ export async function multipleExpForNextLevel(
     );
   }
 
-  const course = await Course.findByPk(courseId);
-  if (!course) {
+  const user_course = await UserCourse.findOne({
+    where: {
+      userId: userId,
+      courseId: courseId,
+    },
+  });
+  if (!user_course) {
     throw new BadRequestError('Course not found');
   }
-  await course.update({
+  await user_course.update({
     nextLevelEffect: effectType,
   });
-  await course.save();
+  await user_course.save();
   return;
 }
 
 export async function multipleGoldForNextLevel(
   effectType: EffectType,
   courseId: string,
+  userId: string,
 ): Promise<void> {
   const validGoldEffects = [EffectType.GoldX2, EffectType.GoldX3, EffectType.GoldX4];
 
@@ -44,13 +51,18 @@ export async function multipleGoldForNextLevel(
     );
   }
 
-  const course = await Course.findByPk(courseId);
-  if (!course) {
+  const user_course = await UserCourse.findOne({
+    where: {
+      userId: userId,
+      courseId: courseId,
+    },
+  });
+  if (!user_course) {
     throw new BadRequestError('Course not found');
   }
-  await course.update({
+  await user_course.update({
     nextLevelEffect: effectType,
   });
-  await course.save();
+  await user_course.save();
   return;
 }
