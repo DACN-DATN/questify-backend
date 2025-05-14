9795b60c19fff5fdcfec8e97cec036ea98aee05b
NHA012 <nha@LAPTOP-QIB2U216.>
2025-05-14 03:53:54 +0000

    hotfix trigger course learning

--- course_learning/src/events/listeners/slide-created-listener.ts
+++ course_learning/src/events/listeners/slide-created-listener.ts
@@ -18,17 +18,6 @@ export class SlideCreatedListener extends Listener<SlideCreatedEvent> {
       return;
     }
 
-    console.log("dcm", {
-      id,
-      title,
-      description,
-      slideNumber,
-      type,
-      imageUrl,
-      videoUrl,
-      answers,
-      challengeId,
-    })
     const slide = Slide.build({
       id,
       title,
