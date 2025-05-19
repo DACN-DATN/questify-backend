9725c5f8e2741cbcef54046acbe239616456d7e6
nha0112 <103880099+nha0112@users.noreply.github.com>
2025-05-19 17:40:48 +0700

    feat: return level info (#92)
    Co-authored-by: NHA012 <nha@LAPTOP-QIB2U216>

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import { LevelService } from '../services/level';
 
 import { currentUser } from '@datn242/questify-common';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+  res.send({
+    currentUser: req.currentUser || null,
+    levelInfo: LevelService.getInstance().getLevelInfo(req.currentUser?.userExp || 0),
+  });
 });
 
 export { router as currentUserRouter };
